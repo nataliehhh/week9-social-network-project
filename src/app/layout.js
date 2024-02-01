e1006b2aa6887e4dbd361a9941f9c7647f25ae46
natalie <hardman.natalie85@gmail.com>
2024-02-01 09:14:27 +0000

    fix header footer

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
+import Footer from "@/comonents/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
